a910ac87ad617e26fec787c52ebe55f604ac9ac3
Bandity <bandity9hard9@gmail.com>
2021-12-01 13:27:39 +0100

    chat-update

--- MedievalWorldQuest/src/modules/index.ts
+++ MedievalWorldQuest/src/modules/index.ts
@@ -3,7 +3,7 @@ import main from './main'
 import defaultGui from '@rpgjs/default-gui' 
 import mobileGui from '@rpgjs/mobile-gui'
 import gamepad from '@rpgjs/gamepad'
-import chat from '../assets/chat';
+import chat from '@rpgjs/chat'
 
 export default [
     main,
