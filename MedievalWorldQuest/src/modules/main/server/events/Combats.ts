08f5b79e092f11c081ba9bdf45e6b14515d9d917
Bandity <bandity9hard9@gmail.com>
2021-12-06 09:26:57 +0100

    Combats v-Beta

--- MedievalWorldQuest/src/modules/main/server/events/Combats.ts
+++ MedievalWorldQuest/src/modules/main/server/events/Combats.ts
@@ -2,7 +2,11 @@ import { RpgEvent, EventData, RpgPlayer} from "@rpgjs/server";
 
 class Combats extends   RpgEvent {
     onAction(player: RpgPlayer, otherPlayer: RpgPlayer, skill:any){
-        player.applyDamage(otherPlayer, skill);
+        let damage = player.applyDamage(otherPlayer, skill);
+        otherPlayer.hp = otherPlayer.hp - damage.damage;
+        if (otherPlayer.hp <=0){
+            otherPlayer.teleport("0,0,0");
+        }
     }
 }
 export{Combats}
