85629cf6091ab51f80e9f99cfa092a25df7db836
Quentin-bp <73823634+Quentin-bp@users.noreply.github.com>
2021-12-13 17:14:36 +0100

    Carte départ : Changements des cascades et début des collisions

--- MedievalWorldQuest/src/modules/main/client/maps/CiteOnirique.ts
+++ MedievalWorldQuest/src/modules/main/client/maps/CiteOnirique.ts
@@ -7,6 +7,8 @@ import { Spritesheet } from '@rpgjs/client'
         fond: require('./assets/bg_bluesky.png'),
         nuage1: require('./assets/bg_cloud1.png'),
         nuage2: require('./assets/bg_cloud2.png'),
+        cascade: require('./assets/cascade2.png'),
+        eauAnime: require('./assets/eauAnime.png')
         // here, you can add other tileset
     }
 })
---
+++ MedievalWorldQuest/src/modules/main/server/maps/tmx/cascade.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="cascade" tilewidth="32" tileheight="32" tilecount="576" columns="32">
+ <image source="../../../client/maps/assets/cascade2.png" width="1024" height="576"/>
+ <tile id="229">
+  <animation>
+   <frame tileid="229" duration="100"/>
+   <frame tileid="237" duration="100"/>
+   <frame tileid="245" duration="100"/>
+   <frame tileid="253" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="230">
+  <animation>
+   <frame tileid="230" duration="100"/>
+   <frame tileid="238" duration="100"/>
+   <frame tileid="246" duration="100"/>
+   <frame tileid="254" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="231">
+  <animation>
+   <frame tileid="231" duration="100"/>
+   <frame tileid="239" duration="100"/>
+   <frame tileid="247" duration="100"/>
+   <frame tileid="255" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
