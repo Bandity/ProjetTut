03a02b01fd33ff940ac9f8636cd2eb0abfa94de3
Bandity <bandity9hard9@gmail.com>
2022-01-13 13:51:44 +0100

    Rajout sortie prog de l'église

--- MedievalWorldQuest/src/modules/main/server/maps/Eglise.ts
+++ MedievalWorldQuest/src/modules/main/server/maps/Eglise.ts
@@ -1,9 +1,15 @@
 import { RpgMap, MapData } from '@rpgjs/server'
+import events  from '../events/Eglise/index'
 
 @MapData({
     id: 'Eglise',
     file: require('./tmx/Eglise.tmx'),
     name : 'Eglise',       
+    events :[ 
+        events.Teleporteur({
+        name: "egliseSortie",
+        nameMap: "MenestrelTown", 
+        }),
+    ],
 })
 export class Eglise extends RpgMap { }
-
