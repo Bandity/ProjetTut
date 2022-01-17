750116b09d0114373557fd6f26840ff9eed78d62
N0umu <74717071+N0umu@users.noreply.github.com>
2022-01-17 18:29:10 +0100

    implementation de la carte de la forge

---
+++ MedievalWorldQuest/src/modules/main/server/maps/tmx/Testforgerie.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Testforgerie" tilewidth="32" tileheight="32" tilecount="300" columns="20">
+ <image source="../Forgerie/Testforgerie.png" width="640" height="480"/>
+</tileset>
--- MedievalWorldQuest/src/modules/main/server/maps/tmx/maison2.tsx
+++ MedievalWorldQuest/src/modules/main/server/maps/tmx/maison2.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="maison2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../maison2.png" width="512" height="512"/>
+ <image source="maison2.png" width="512" height="512"/>
+ <tile id="163">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
