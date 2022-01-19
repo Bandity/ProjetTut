30a48b77d79242880ad73bdd932f57065eefdd6a
Bandity <bandity9hard9@gmail.com>
2022-01-19 11:37:59 +0100

    Monsters and combats changes

--- MedievalWorldQuest/src/modules/main/server/events/Monsters.ts
+++ MedievalWorldQuest/src/modules/main/server/events/Monsters.ts
@@ -1,5 +1,5 @@
 import { Enemy } from '@rpgjs/database'
-import { RpgEnemy, Presets } from '@rpgjs/server'
+import { RpgEnemy, Presets, RpgEvent } from '@rpgjs/server'
 import { Shield } from '@rpgjs/starter-kit/src/server/database/armors/shield'
 import { Sword } from '@rpgjs/starter-kit/src/server/database/weapons/sword'
 import {RpgPlayer , EventData} from '@rpgjs/server'
@@ -19,12 +19,24 @@ const { MAXHP, STR } = Presets
     },
     startingEquipment: []
 })
-
-export class Monster extends RpgEnemy {
+@EventData({
+    name: 'Monster',
+    //mode: EventMode.Scenario,
+    hitbox: {
+        width: 32,
+        height: 16
+    
+    }
+})
+export class Monster extends RpgEvent {
     async onInit(player: RpgPlayer) {
+        
         this.setGraphic("witch")
         //this.changeMap("MenestrelTown")
-        this.teleport({x: 300, y: 300, z: 0})
 
+    }
+    async onAction(player: RpgPlayer){
+        console.log("as")
+        player.hp =-1;
     }
 }   
