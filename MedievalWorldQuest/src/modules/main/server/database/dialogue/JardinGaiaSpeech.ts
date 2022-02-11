dfecd851c6cf22cbaf9290460c8d7c676ba98be5
Quentin-bp <73823634+Quentin-bp@users.noreply.github.com>
2022-02-11 18:04:49 +0100

    Rajout histoire principale jardin de gaia -> Terminé

---
+++ MedievalWorldQuest/src/modules/main/server/database/dialogue/JardinGaiaSpeech.ts
@@ -0,0 +1,45 @@
+export const speech = {
+
+    textGardeJardin : [
+        "… Vous êtes donc venu. Je vais vous guider à notre maître, il a énormément de chose à vous dire. "
+    ],
+
+
+    textMaitreJardins : [ 
+   " Il semblerait que vous soyez finalement venu.",
+    "Pour répondre à quelques-unes de vos questions, sachez qu’aucun être vivant ne peut percevoir cet endroit.",
+    "Avant que l’attaque du roi n’atteigne son but, un inconnu nommé Surt est apparu et a usé d’une magie étrange pour nous protéger." ,
+    "Cette magie a fragmenté les Jardins en plusieurs parties et les a téléportées partout sur la surface de la planète, tout en les rendant invisible et impossible d’accès.",
+    "Ce genre de magie dépassait l’entendement, et avec cette force, l’inconnu aurait pu tuer le roi en un rien de temps.",
+    "Cependant… Il n’a rien fait de tel. Il avait tout de même anticipé que le roi ne s’arrêterais pas là, et a donc proclamé ceci : ",
+    "« En mon nom, Surt, je vais te dire quelques mots.",
+    "Un jour, un enfant naîtra. A tous, il apportera un espoir, mais pour toi, cet espoir ne sera que peur.",
+    "Dans ses yeux tu verras l’honnêteté et la naïveté de ce monde. Cet enfant en suivant son destin, grandira et te fera face plus d’une fois.",
+    "Contre lui, aucune de tes magies et aucune de tes lames ne le tueront. Il remportera toutes ses batailles, et face à toi, il montrera ce que la peur signifie.",
+    "Il apportera une nouvelle ère à cette planète, il engloutira la peur que tu instaureras, et fera germer la graine de l’espoir en ce monde.",
+    "Ce monde a besoin de grandir. Toi qui estimes qu’il est de ton devoir de régner sur les hommes, cet enfant te montrera qu’il est possible de faire autrement.",
+    "Il ne règnera non pas sur les hommes, mais avec les hommes, cette différence changera ce monde que tu corromps.",
+    "Il sera le dernier espoir que le monde attend.",
+    "Il sera Elpis. »",
+    "...",
+    "Après avoir dit ça, il s’est téléporté, et nous n’avons plus jamais entendu parler de lui.",
+    "Cependant, il a laissé dans chacun des fragments du Jardin de Gaia, un morceau de pouvoir.",
+    "Nos ancêtres ont fait des recherches et ont découvert peu de temps après avoir compris le sortilège que Surt avait lancé aux jardins, les effets de ce pouvoir.",
+    "Il s’agit d’un pouvoir vraiment étrange, c’est une magie qui engloutie absolument tout, à une exception près." ,
+    "Personne ne peut utiliser la force de cette magie, pas même toi.",
+    "Cependant, cette magie n’a pas vocation à être utilisée comme une arme, mais comme un soutient.",
+    "En réalité, nous ne comprenons toujours pas les vrais effets de cette magie mais… Ils sont liés au temps.",
+    "On dirait que la magie permet de montrer à son possesseur son passé, et son destin.",
+    "Mais il faut faire attention. Les êtres vivants n’ont pas été conçu pour voir leur destin.",
+    "Nous allons donc vous donner ce pouvoir puisqu’il vous est destiné, mais comme je vous l’ai dit, il ne s’utilise pas.",
+    "Nous ne savons pas comment ses effets peuvent s’enclencher, alors il faut faire attention.",
+    "Je pense que vous commencez maintenant à comprendre qui vous êtes, mais ce n’est que le début.",
+    "Vous devriez vous reposez ici avant de repartir. Vous avez maintenant le choix. ",
+    "Soit vous désirez repartir à votre vie telle que vous la meniez avant votre arrivée aux Jardins, soit vous voulez en apprendre plus sur vous, auquel cas vous devez devenir plus fort.",
+    "Il existe un endroit dans les jardins, vous permettant d’apprendre à maitriser vos armes et vos magies.",
+    "Elle se situe au-delà des Montagnes de Chione, et l’accès y est très difficile… Mais c’est votre prochaine destination.",
+    "N’oubliez cependant jamais, ce destin que vous avez, n’est qu’un destin. Il ne tient qu’à vous de modifier ses routes et ses chemins, et d’en tracer un autre.",
+    "Je vous souhaite maintenant une bonne route, et puissiez vous trouver ce que vous désirez.",
+    ],
+    
+}
